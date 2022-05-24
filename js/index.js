9bf5b3a8cfec0cc449b97ac85ba1dfd44b23948a
Johan Meneses <johanmeci03@gmail.com>
2022-05-23 22:08:26 -0500

    Add header mobile style

--- js/index.js
+++ js/index.js
@@ -1,1 +1,8 @@
 console.log('load script');
+const btnMenu = document.querySelector('#iconMenu');
+const navbar = document.querySelector('#navbar');
+
+btnMenu.addEventListener('click', (e) => {
+  e.currentTarget.classList.toggle('active');
+  navbar.classList.toggle('active');
+});
